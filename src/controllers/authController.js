ec6c8f52e1d011b2bfc8f1176b0f32568649f01a
khojiakbargofurov <harry.gafurov@gmail.com>
2025-12-15 00:10:08 +0500

    push

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,7 @@ const User = require('../models/userModel');
 
 const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
+        expiresIn: process.env.JWT_EXPIRES_IN || '90d',
     });
 };
 
